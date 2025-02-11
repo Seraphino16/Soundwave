b2ef82ba68f85dbe6f8363bf1888913442c5797e
stephpiquette <svaillant.e2c@gmail.com>
2025-02-11 16:49:06 +0100

    refactor: isolatedModules set to false in tsconfig to avoid errors due to empty files (must be turned on later on the project)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
 // @ts-ignore
 import React from 'react';
 import './assets/styles/App.css';
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-red-500">
-        tailwindcss
-      </h1>
-    </div>
+    <Home />
   );
 }
