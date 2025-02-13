dcc097d1bc5a15ac499260ecdf14344122d84fe6
emilie <emilie.le-lan@epitech.eu>
2025-02-13 15:52:41 +0100

    feat(database): connect the database and create the user collection
    - import the database url from AppService
    - import UserModule from UserModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
     imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
+        ConfigModule.forRoot({ isGlobal: true }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                uri: configService.get<string>('MONGODB_URI'),
+            }),
+            inject: [ConfigService],
         }),
+        UserModule,
     ],
     controllers: [AppController],
     providers: [AppService],
 })
 export class AppModule {}
+
+
 
