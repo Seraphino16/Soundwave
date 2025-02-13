855ba1f45a5591405ff76cd4088a439ac4006525
Zoé Pilia <61697702+zpilia@users.noreply.github.com>
2025-02-13 16:53:06 +0100

    [feature/listing-albums] New album list retrieval via Spotify API added (Back)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SpotifyService } from './spotify/spotify.service';
+import { SpotifyController } from './spotify/spotify.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [SpotifyController],
+  providers: [SpotifyService],
 })
 export class AppModule {}
---
+++ server/src/spotify/spotify.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
+
+@Injectable()
+export class SpotifyService {
+  private readonly spotifyApiUrl: string;
+  private readonly clientId: string;
+  private readonly clientSecret: string;
+
+  constructor(private configService: ConfigService) {
+    // Récupérer les informations à partir des variables d'environnement
+    this.spotifyApiUrl = <string>(
+      this.configService.get<string>('SPOTIFY_API_URL')
+    );
+    this.clientId = <string>this.configService.get<string>('SPOTIFY_CLIENT_ID');
+    this.clientSecret = <string>(
+      this.configService.get<string>('SPOTIFY_CLIENT_SECRET')
+    );
+  }
+
+  // Méthode pour récupérer les albums
+  async getNewReleases(limit: number, offset: number) {
+    try {
+      // Récupérer un token d'accès avec le client ID et le secret
+      const authResponse = await axios.post(
+        'https://accounts.spotify.com/api/token',
+        new URLSearchParams({
+          grant_type: 'client_credentials',
+        }),
+        {
+          headers: {
+            Authorization: `Basic ${Buffer.from(`${this.clientId}:${this.clientSecret}`).toString('base64')}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        },
+      );
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+      const accessToken = authResponse.data.access_token;
+
+      // Appeler l'API des nouvelles sorties avec le token d'accès
+      const response = await axios.get(
+        `${this.spotifyApiUrl}/browse/new-releases`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+          params: {
+            limit: limit,
+            offset: offset,
+          },
+        },
+      );
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return response.data; // Retourner les données de la réponse API
+    } catch (error) {
+      console.error('Erreur lors de la récupération des albums:', error);
+      throw new Error('Erreur lors de la récupération des albums');
+    }
+  }
+}
