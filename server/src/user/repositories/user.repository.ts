ab1cee71df9928284dfbc383158ddc60b0b3a302
emilie <emilie.le-lan@epitech.eu>
2025-02-20 18:11:16 +0100

    feat: add all user fields in the create method

--- server/src/user/repositories/user.repository.ts
+++ server/src/user/repositories/user.repository.ts
@@ -6,10 +6,43 @@ import { User } from '../entities/user.entity'
 @Injectable()
 export class UserRepository {
     constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
-    async create(email: string, password: string, googleId?: string): Promise<User> {
-        const newUser = new this.userModel({ email, password, googleId });
+    async create(
+        email: string,
+        password: string,
+        pseudo: string,
+        username: string,
+        birthdate: Date,
+        googleId?: string,
+        facebookId?: string,
+        twitterId?: string,
+        deezerId?: string,
+        spotifyId?: string,
+        roles: string[] = ['USER'],
+        verification_token?: string,
+        is_verified: boolean = false,
+        is_active: boolean = true
+    ): Promise<User> {
+        const newUser = new this.userModel({
+            email,
+            password,
+            pseudo,
+            username,
+            birthdate,
+            googleId,
+            facebookId,
+            twitterId,
+            deezerId,
+            spotifyId,
+            roles,
+            verification_token,
+            is_verified,
+            is_active,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        });
         return newUser.save();
     }
+
 
     async findByEmail(email: string): Promise<User | null> {
         return this.userModel.findOne({ email }).exec();
