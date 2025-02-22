5caed5d88fe70f948f3cec14c510e103b1df75cf
emilie <emilie.le-lan@epitech.eu>
2025-02-22 11:52:15 +0100

    feat: create user success class for retourned user success messages

---
+++ server/src/user/success/user.success.ts
@@ -0,0 +1,22 @@
+export class UserSuccess {
+    status: string;
+    code: number;
+    message: string;
+    data: any;
+
+    constructor(status: string, code: number, message: string, data: any = null) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    static createSuccess(status: string, code: number, message: string, data: any = null) {
+        return new UserSuccess(status, code, message, data);
+    }
+
+    static userCreated(data: any) {
+        return UserSuccess.createSuccess('success', 201, 'Utilisateur créé avec succès', data);
+    }
+
+}
