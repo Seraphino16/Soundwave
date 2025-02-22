f16fd3780cf0c83a6d9e9ff1b1aff258fd2a7f21
emilie <emilie.le-lan@epitech.eu>
2025-02-22 11:54:54 +0100

    feat: add user repository in user module provider

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -2,16 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UserRepository } from './repositories/user.repository';
 import { UserSchema } from './entities/user.entity';
 
 @Module({
     imports: [
-        MongooseModule.forFeature([
-            { name: 'User', schema: UserSchema },
-        ]),
+        MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
     ],
-    providers: [UserService],
+    providers: [UserService, UserRepository],
     controllers: [UserController],
 })
 export class UserModule {}
+
 
