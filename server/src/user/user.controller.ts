0cab8360fb424c54b2c71d51a23edea792852bdd
emilie <emilie.le-lan@epitech.eu>
2025-02-22 11:53:16 +0100

    feat: add user controller for create user

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,5 +1,24 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException, ConflictException } from '@nestjs/common';
 import { UserService } from './user.service';
-import { User } from './entities/user.entity';
-@Controller('user')
-export class UserController {}
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserErrors } from './errors/user.errors';
+import { UserSuccess } from './success/user.success';
+@Controller('users')
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Post('create')
+    async create(@Body() createUserDto: CreateUserDto): Promise<UserSuccess> {
+        try {
+            const userId = await this.userService.createUser(createUserDto);
+
+            return UserSuccess.userCreated(userId);
+        } catch (error) {
+            if (error instanceof BadRequestException || error instanceof ConflictException) {
+                throw error;
+            }
+
+           throw new BadRequestException(UserErrors.unknownError().message);
+        }
+    }
+}
