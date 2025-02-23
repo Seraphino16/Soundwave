f8afdfda19d82051857eeda27dea25b18a6a6a36
emilie <emilie.le-lan@epitech.eu>
2025-02-23 14:00:49 +0100

    feat(token): add errors for token class
    - invalidToken
    - expiredToken
    - malformedToken
    - internalServerError

---
+++ server/src/token/errors/token.errors.ts
@@ -0,0 +1,39 @@
+export class TokenErrors {
+  status: string;
+  code: number;
+  message: string;
+  data?: any;
+
+  constructor(status: string, code: number, message: string, data?: any) {
+    this.status = status;
+    this.code = code;
+    this.message = message;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    this.data = data;
+  }
+
+  static createError(
+    status: string,
+    code: number,
+    message: string,
+    data?: any,
+  ): TokenErrors {
+    return new TokenErrors(status, code, message, data);
+  }
+
+  static invalidToken(data?: any): TokenErrors {
+    return TokenErrors.createError('error', 401, 'Le token est invalide', data);
+  }
+
+  static expiredToken(data?: any): TokenErrors {
+    return TokenErrors.createError('error', 401, 'Le token à expiré', data);
+  }
+
+  static malformedToken(data?: any): TokenErrors {
+    return TokenErrors.createError('error', 400, 'Le token est malformé', data);
+  }
+
+  static internalServerError(message: string, data?: any): TokenErrors {
+    return TokenErrors.createError('error', 500, message, data);
+  }
+}
