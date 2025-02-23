4d46cecf8991bc591dec159ac8c6bb4ecb76b548
emilie <emilie.le-lan@epitech.eu>
2025-02-23 14:10:44 +0100

    feat(mailer): add post method for send an email validation

--- server/src/mailer/mailer.controller.ts
+++ server/src/mailer/mailer.controller.ts
@@ -3,4 +3,17 @@ import { MailerService } from './mailer.service';
 import { MailerErrors } from './errors/mailer.errors';
 
 @Controller('mailer')
-export class MailerController {}
+export class MailerController {
+  constructor(private readonly mailerService: MailerService) {}
+
+  @Post('send-validation-email')
+  sendValidationEmail(@Body() body: { email: string; token: string }): void {
+    const { email, token } = body;
+
+    try {
+      return this.mailerService.sendValidationEmail(email, token);
+    } catch {
+      throw new BadRequestException(MailerErrors.emailNotSent().message);
+    }
+  }
+}
