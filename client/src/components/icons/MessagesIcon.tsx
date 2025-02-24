3a38ebdf8b5405896d2f6f253365328882369d28
stephpiquette <svaillant.e2c@gmail.com>
2025-02-24 16:56:47 +0100

    refactor: moved icons into new components

---
+++ client/src/components/icons/MessagesIcon.tsx
@@ -0,0 +1,23 @@
+/**
+ * @description Icone d'une bulle de message
+ * @author SoundWave
+ */
+
+const MessagesIcon = () => (
+    <svg
+        width="28"
+        height="24"
+        viewBox="0 0 28 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M8.4 7.2H18.2M8.4 12H14M13.6348 16.5913L7.79135 21.6V16.5913H5.6C4.05365 16.5913 2.8 15.5168 2.8 14.1913V4.8C2.8 3.47454 4.05365 2.4 5.6 2.4H22.4C23.9464 2.4 25.2 3.47454 25.2 4.8V14.1913C25.2 15.5168 23.9464 16.5913 22.4 16.5913H13.6348Z"
+            stroke="#93D9D6"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+    </svg>
+);
+export default MessagesIcon;
---
+++ client/src/components/icons/ProfileIcon.tsx
@@ -0,0 +1,23 @@
+/**
+ * @description Icone d'un bonhomme représentant la page de profil d'un utilisateur (pourra être adaptée à la photo de profil de l'utilisateur prochainement)
+ * @author SoundWave
+ */
+
+const ProfileIcon = () => (
+    <svg
+        width="27"
+        height="24"
+        viewBox="0 0 27 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M22.9494 21.6L22.9498 18.0003C22.95 16.012 21.1367 14.4 18.8998 14.4H8.10063C5.86405 14.4 4.05088 16.0116 4.05063 17.9996L4.05017 21.6M17.5502 6C17.5502 7.98825 15.7369 9.6 13.5002 9.6C11.2634 9.6 9.45017 7.98825 9.45017 6C9.45017 4.0118 11.2634 2.4 13.5002 2.4C15.7369 2.4 17.5502 4.0118 17.5502 6Z"
+            stroke="#93D9D6"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+    </svg>
+);
+export default ProfileIcon;
