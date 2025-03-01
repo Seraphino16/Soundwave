9ddc92a316f705fd255ce6e9eb46d7804e3dd9a9
emilie <emilie.le-lan@epitech.eu>
2025-03-01 21:09:42 +0100

    feat(auth): import AuthController and AuthModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -10,6 +10,8 @@ import { MailerModule } from './mailer/mailer.module';
 import { TokenService } from './token/token.service';
 import { TokenController } from './token/token.controller';
 import { TokenModule } from './token/token.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
