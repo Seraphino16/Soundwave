e9a3b04f40850c377742a95fb3234cebd2878a25
Zoé Pilia <61697702+zpilia@users.noreply.github.com>
2025-03-07 21:33:22 +0100

    [feat/FRONT] Update style of Album List

--- client/src/components/cards/AlbumCard.tsx
+++ client/src/components/cards/AlbumCard.tsx
@@ -9,7 +9,7 @@ const AlbumCard: React.FC<AlbumProps> = ({ title, coverImage }) => {
     return (
         <div className="bg-white shadow-lg rounded-lg p-4 w-60">
             <img src={coverImage} alt={title} className="w-full h-48 object-cover rounded-md" />
-            <h2 className="text-lg font-semibold mt-2 text-center">{title}</h2>
+            <h2 className="text-lg font-medium mt-2 text-center">{title}</h2>
         </div>
     );
 };
