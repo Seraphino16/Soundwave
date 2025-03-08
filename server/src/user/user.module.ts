f4a240a88562ea8f5344cb28012bb52aace8fb46
emilie <emilie.le-lan@epitech.eu>
2025-03-08 15:58:49 +0100

    feat(auth): import token and guard

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -6,14 +6,17 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserRepository } from './repositories/user.repository';
 import { UserSchema } from './entities/user.entity';
+import { TokenModule } from '../token/token.module';
+import { JwtAuthGuard } from '../auth/jwt-auth/jwt-auth.guard';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
     MailerModule,
     HttpModule,
+    TokenModule,
   ],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, JwtAuthGuard],
   controllers: [UserController],
   exports: [UserRepository],
 })
