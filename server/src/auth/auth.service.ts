874999495702d36d79a7f01a7f28d03dafaddb00
emilie <emilie.le-lan@epitech.eu>
2025-03-08 12:17:41 +0100

    feat(auth): import modules : UnauthorizedException, UserRepository, TokenService, AuthErrors, AuthDto

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserRepository } from '../user/repositories/user.repository';
+import { TokenService } from '../token/token.service';
+import { AuthErrors } from './errors/auth.errors';
+import * as bcrypt from 'bcryptjs';
+import { AuthDto } from './dto/auth-dto';
 
 @Injectable()
 export class AuthService {}
