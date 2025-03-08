c1a7ab21cec1d9d8018f619b28be91fd8bb8377c
emilie <emilie.le-lan@epitech.eu>
2025-03-08 22:33:33 +0100

    feat(user): add dto for user infos create

---
+++ server/src/user/dto/create-user-infos.ts
@@ -0,0 +1,51 @@
+import {
+  IsString,
+  IsOptional,
+  IsArray,
+  IsNumber,
+  IsDate,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateUserInfosDto {
+  @IsNumber()
+  id: number;
+
+  @IsNumber()
+  user_id: number;
+
+  @IsOptional()
+  @IsString()
+  profile_picture?: string;
+
+  @IsOptional()
+  @IsString()
+  banner_picture?: string;
+
+  @IsOptional()
+  @IsString()
+  bio?: string;
+
+  @IsOptional()
+  @IsString()
+  location?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  musicStyle?: string[];
+
+  @IsOptional()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Object)
+  socialLinks?: { [network: string]: string };
+
+  @IsDate()
+  createdAt: Date;
+
+  @IsDate()
+  updatedAt: Date;
+}
