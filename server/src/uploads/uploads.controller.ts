15b47b6753766873ff23258f195374000f7f4f69
emilie <emilie.le-lan@epitech.eu>
2025-03-09 00:17:02 +0100

    feat(uploads): add uploads controller

---
+++ server/src/uploads/uploads.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadsService } from './uploads.service';
+import { Express } from 'express';
+
+@Controller('uploads')
+export class UploadsController {
+  constructor(private readonly fileUploadService: UploadsService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.fileUploadService.handleFileUpload(file);
+  }
+}
