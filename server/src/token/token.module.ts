ce7aca2cb4138aa6399b782181f67fc44e77d1ee
emilie <emilie.le-lan@epitech.eu>
2025-03-08 12:16:28 +0100

    feat(auth): export token service

--- server/src/token/token.module.ts
+++ server/src/token/token.module.ts
@@ -7,5 +7,6 @@ import { ConfigModule } from '@nestjs/config';
   imports: [ConfigModule],
   providers: [TokenService],
   controllers: [TokenController],
+  exports: [TokenService],
 })
 export class TokenModule {}
