2b54d18f505ca926cdf7afe06f796d646d3c1f6e
emilie <emilie.le-lan@epitech.eu>
2025-03-08 11:05:48 +0100

    feat(auth): add auth errors for missing required fields

---
+++ server/src/auth/errors/auth.errors.ts
@@ -0,0 +1,41 @@
+export class AuthErrors {
+  status: string;
+  code: number;
+  message: string;
+  data?: any;
+
+  constructor(status: string, code: number, message: string, data?: any) {
+    this.status = status;
+    this.code = code;
+    this.message = message;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    this.data = data;
+  }
+
+  static createError(
+    status: string,
+    code: number,
+    message: string,
+    data?: any,
+  ): AuthErrors {
+    return new AuthErrors(status, code, message, data);
+  }
+
+  static missingLogin(data?: any): AuthErrors {
+    return AuthErrors.createError(
+      'error',
+      400,
+      "L'email ou le username sont requis pour se connecter",
+      data,
+    );
+  }
+
+  static missingPassword(data?: any): AuthErrors {
+    return AuthErrors.createError(
+      'error',
+      400,
+      'Votre mot de passe est requis pour vous connecter',
+      data,
+    );
+  }
+}
