7bb876579638dc55d12423384d8cfb7139714c17
emilie <emilie.le-lan@epitech.eu>
2025-03-08 12:18:11 +0100

    feat(auth): create class for authentication with login method

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -6,4 +6,62 @@ import * as bcrypt from 'bcryptjs';
 import { AuthDto } from './dto/auth-dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly tokenService: TokenService,
+  ) {}
+
+  async login(loginDto: AuthDto) {
+    const { username, email, password } = loginDto;
+
+    if (!username && !email) {
+      throw new UnauthorizedException(AuthErrors.missingLogin().message);
+    }
+
+    if (!password) {
+      throw new UnauthorizedException(AuthErrors.missingPassword().message);
+    }
+
+    const user = await this.getUserByUsernameOrEmail(username, email);
+
+    if (!user) {
+      throw new UnauthorizedException(AuthErrors.invalidCredentials().message);
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException(AuthErrors.invalidCredentials().message);
+    }
+
+    if (!user.is_verified || !user.is_active) {
+      throw new UnauthorizedException(AuthErrors.accountNotActivated().message);
+    }
+
+    const token = this.tokenService.generateLoginToken({
+      username: user.username,
+      id: user.id,
+      email: user.email,
+    });
+
+    // const socketId = 'dummySocketId';
+    //this.socketService.registerUserSocket(socketId, user.id);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const userWithoutPassword = { ...user.toObject(), password: undefined };
+
+    return {
+      token,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      user: userWithoutPassword,
+    };
+  }
+
+  private async getUserByUsernameOrEmail(username: string, email: string) {
+    if (email) {
+      return this.userRepository.findByEmail(email);
+    }
+    return this.userRepository.findByUsername(username);
+  }
+}
