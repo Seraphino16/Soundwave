2f294fc5212da72aa5804ce4627eeb97d9a466bf
emilie <emilie.le-lan@epitech.eu>
2025-03-08 12:18:50 +0100

    feat(auth): import modules : AuthController, AuthService, UserModule, TokenModule

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserModule } from '../user/user.module';
+import { TokenModule } from '../token/token.module';
 
 @Module({})
 export class AuthModule {}
