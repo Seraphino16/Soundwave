37053f842cbb0fdf82bf0e5fa1b27dcb3b5dccdf
emilie <emilie.le-lan@epitech.eu>
2025-04-07 14:48:28 +0200

    feat: add Api Property on create-user-infos.dto

--- server/src/user/dto/create-user-infos.dto.ts
+++ server/src/user/dto/create-user-infos.dto.ts
@@ -8,41 +8,52 @@ import {
   ValidateNested,
 } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserInfosDto {
+
+  @ApiProperty()
   @IsInt()
   user_id: number;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   profile_picture?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   banner_picture?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   bio?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   location?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   musicStyle?: string[];
 
+  @ApiProperty()
   @IsOptional()
   @IsObject()
   @ValidateNested()
   @Type(() => Object)
   socialLinks?: { [network: string]: string };
 
+  @ApiProperty()
   @IsDate()
   createdAt?: Date;
 
+  @ApiProperty()
   @IsDate()
   updatedAt?: Date;
 }
