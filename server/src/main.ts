4978002a38dd74846ee8e196e022407992578add
emilie <emilie.le-lan@epitech.eu>
2025-04-07 14:43:25 +0200

    feat: add swagger api for SoundWave app

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,15 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const swaggerConfig = new DocumentBuilder()
+      .setTitle('SoundWave api')
+      .setDescription('The SoudWave api documentation')
+      .setVersion('1.0')
+      .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 5001);
 }
 bootstrap();
