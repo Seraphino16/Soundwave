37ba7e0690edbdd10a02f818a22817f5c0bff54b
emilie <emilie.le-lan@epitech.eu>
2025-05-07 11:19:35 +0200

    refactor(user): refactor create methode, remove call to the mailer and token controller, call the mailer and token services

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -35,47 +35,35 @@ export class UserController {
   ) {}
 
   @Post('create')
-  async create(@Body() createUserDto: CreateUserDto): Promise<UserSuccess> {
+  async create(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<UserSuccess | UserErrors> {
     try {
       const userId = await this.userService.createUser(createUserDto);
 
-      const generateTokenDto = {
+      const activationToken = this.tokenService.generateEmailValidationToken({
         email: createUserDto.email,
         username: createUserDto.username,
         id: userId,
-      };
+      });
 
-
-      const tokenResponse = await firstValueFrom(
-        this.httpService.post(
-          'http://localhost:5001/token/generate-email-validation',
-          generateTokenDto,
-        ),
-      );
-
-      const token = tokenResponse.data;
-      await this.userService.saveValidationToken(userId, token);
-
-      await firstValueFrom(
-        this.httpService.post(
-          'http://localhost:5001/mailer/send-validation-email',
-          {
-            email: createUserDto.email,
-            token: token,
-          },
-        ),
-      );
+      await this.mailerService.sendValidationEmail({
+        to: createUserDto.email,
+        username: createUserDto.username,
+        token: activationToken,
+      });
 
       return UserSuccess.userCreated(userId);
     } catch (error) {
       if (
-        error instanceof BadRequestException ||
-        error instanceof ConflictException
+        error instanceof UserErrors ||
+        error instanceof MailerErrors ||
+        error instanceof TokenErrors
       ) {
         throw error;
       }
 
-      throw new BadRequestException(UserErrors.unknownError().message);
+      throw UserErrors.unknownError();
     }
   }
 
