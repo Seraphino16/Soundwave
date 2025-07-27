2afe72ea261007707910502cde072b89d02e062e
emilie <emilie.le-lan@epitech.eu>
2025-07-27 17:33:58 +0200

    feat(google): add route to redirect to google auth callback

--- server/src/google/google.controller.ts
+++ server/src/google/google.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('google')
-export class GoogleController {}
+export class GoogleController {
+  @Get('create/google')
+  redirectToGoogleAuth() {
+    const baseUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
+    const params = new URLSearchParams({
+      client_id: process.env.GOOGLE_CLIENT_ID!,
+      redirect_uri: 'http://localhost:5001/users/create/google/callback',
+      response_type: 'code',
+      scope: 'openid email profile',
+      access_type: 'offline',
+      prompt: 'consent',
+    });
+
+    return { url: `${baseUrl}?${params.toString()}` };
+  }
+}
