9b81963249a640b730fc8864345bc25b6e3a9bec
emilie <emilie.le-lan@epitech.eu>
2025-08-01 16:44:48 +0200

    feat(jwt) add interface for jwt payload and add email in user login token

--- server/src/token/token.service.ts
+++ server/src/token/token.service.ts
@@ -14,8 +14,8 @@ export class TokenService {
   }
 
   generateLoginToken(generateTokenDto: GenerateTokenDto): string {
-    const { username, id } = generateTokenDto;
-    const payload = { username, id };
+    const { username, id, email } = generateTokenDto;
+    const payload = { username, id, email };
     return this.jwtService.sign(payload, { expiresIn: '24h' });
   }
 
