b78df610023f2985b23d3259fe6ce1b4f0c831ba
emilie <emilie.le-lan@epitech.eu>
2025-11-12 22:13:02 +0100

    feat(events) : add controller, service, module and dto

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -22,6 +22,8 @@ import { UtilsModule } from './utils/utils.module';
 import { RatingsModule } from './ratings/ratings.module';
 import { ReviewsModule } from './reviews/reviews.module';
 import { WavesModule } from './waves/waves.module';
+import { EventsModule } from './events/events.module';
+import { DiscussionsModule } from './discussions/discussions.module';
 
 @Module({
   imports: [
@@ -44,6 +46,8 @@ import { WavesModule } from './waves/waves.module';
     RatingsModule,
     ReviewsModule,
     WavesModule,
+    EventsModule,
+    DiscussionsModule,
   ],
   controllers: [
     AppController,
---
+++ server/src/events/errors/events.errors.ts
@@ -0,0 +1,4 @@
+export const EventsErrors = {
+    MISSING_API_KEY: 'Missing Setlist.fm API key. Please define SETLISTFM_API_KEY in environment.',
+    FAILED_TO_FETCH: 'Failed to fetch events from Setlist.fm.',
+};
